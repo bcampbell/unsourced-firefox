aea5daa4cc082cb8b36af267a69faf68f3bafbd0
Ben Campbell <ben@scumways.com>
2012-10-19 14:07:20 +1300

    add some text searching to look for indicators of missing sources

--- data/content.js
+++ data/content.js
@@ -30,7 +30,79 @@ function augmentArticle(artDetails) {
       var html = render(label_template, label);
       $('#unsourced-overlay').append(html);
     }
-  } else {
+  }
+}
+
+
+
+// recursively search node for strings.
+// returns array of matches, of form [node,startpos,endpos,matchingstring]
+/*
+Based on jquery.highlight work by:
+Marshal <beatgates@gmail.com>
+Johann Burkard <http://johannburkard.de> <mailto:jb@eaio.com>
+
+MIT license.
+ 
+*/
+function searchHTML(node,strings) {
+  function reQuote(str) {
+    // escape special regexp chars
+    return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+  };
+
+    // build strings into a single regexp
+    var pats = [];
+    for( var i=0; i<strings.length; ++i ) {
+      pats.push( '(?:' + reQuote(strings[i]) + ')' );
+    }
+    var pattxt = "(" + pats.join('|') + ")";
+    var pat = new RegExp(pattxt,"gi"); 
+
+    function inner(node) {
+      var results = [];
+        if (node.nodeType === 3) { // 3 - Text node
+          // NOTE: this relies on regexp having parentheses (ie capturing),
+          // so the matching part shows up in the list returned by split()
+          m = node.data.split(pat);
+          var i=0;
+          var pos=0;
+          while((i+1)<m.length) {
+            // every second item will be matching text
+            pos += m[i].length;
+            var end = pos + m[i+1].length;
+            results.push( [node, pos, end, m[i+1]] );
+            pos = end;
+            i+=2;
+          }
+        } else if (node.nodeType === 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) { // 1 - Element node
+            for (var i = 0; i < node.childNodes.length; i++) {
+                results.push.apply( results, inner(node.childNodes[i]));
+            }
+        }
+        return results;
+    }
+ 
+    return inner(node);
+};
+
+
+
+function checkTelltaleSigns() {
+
+  var indicators = ["scientists have", "scientists say",  "paper published", "research suggests", "latest research", "researchers", "the study"]
+
+/* other possibilities:
+  "according to a new study"
+  "the study"
+  "findings"
+
+*/
+
+  var hits = searchHTML(document.body, indicators);
+  if(hits.length>0) {
+    // looks like sourcing is needed...
+    console.log("TELLTALE SIGNS!");
   }
 }
 
